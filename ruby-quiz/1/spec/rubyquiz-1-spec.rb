4d929d863d518fafd72111733c00843a687e95ed
habibnahas <habibnahas@gmail.com>
2010-08-02 01:12:42 +0530

    modified tests to work with solution code inside RubyQuiz1 module tests now work with static functions off the Crypto library rather than instance variables within it added new tests to convert and encrypt a message added new pending test to decrypt a message added 2 new tests to describe a deck, it having 54 cards and having cards in order added a pending test to randomize the deck

--- ruby-quiz/1/spec/rubyquiz-1-spec.rb
+++ ruby-quiz/1/spec/rubyquiz-1-spec.rb
@@ -3,31 +3,53 @@ $:.unshift File.join(File.expand_path(File.dirname(__FILE__)), "..") #include pa
 
 require 'rubyquiz-1'
 
-describe "Crypto" do
+describe "RubyQuiz1::Crypto" do
   it "should be able to prune and group characters" do
     message = "Code in Ruby, live longer!"
-    crypto_1 = Crypto.new(message)
-    crypto_1.group_message.should eql(["CODEI","NRUBY","LIVEL","ONGER"])
+    RubyQuiz1::Crypto.group_message(message).should eql(["CODEI","NRUBY","LIVEL","ONGER"])
   end
   it "should group an array with non-boundary length" do
     message = "Code in Ruby, live long and prosper!"
-    crypto_1 = Crypto.new(message)
-    crypto_1.group_message.should eql(["CODEI","NRUBY","LIVEL","ONGAN","DPROS","PER"])
+    RubyQuiz1::Crypto.group_message(message).should eql(["CODEI","NRUBY","LIVEL","ONGAN","DPROS","PER"])
   end  
   it "should prune a garbage string to an empty array" do
     message = "0&(#&#&987304(^989&(7%_()*&"
-    crypto_1 = Crypto.new(message)
-    crypto_1.group_message.should eql([])
+    RubyQuiz1::Crypto.group_message(message).should eql([])
+  end
+  it "should convert message to numbers" do    
+  end
+  it "should encrypt a message correctly" do
+    message = "Code in Ruby, live longer!"
+    solitaire_key = "DWJXH YRFDG TMSHP UURXJ"
+    encrypted_msg = RubyQuiz1::Crypto.encrypt(message,solitaire_key)
+    encrypted_msg.should === ["GLNCQ","MJAFF","FVOMB","JIYCB"].join
+  end
+  it "should decrypt a message correctly" do
+    pending
   end  
 end
 
-describe "Deck" do
-  it "should have 52 cards"
-  it "should have 52 cards in order Clubs,Diamonds,Hearts,Spades,Joker under default configuration" do
-    deck = Deck.new
-    dec.cards
+
+describe "RubyQuiz1::Deck" do
+  it "should have 54 cards" do
+    deck = RubyQuiz1::Deck.new
+    deck.cards.count.should eql(54)
+  end
+  it "should have cards in order Clubs,Diamonds,Hearts,Spades,Joker under default configuration" do
+    deck = RubyQuiz1::Deck.new
+    deck.cards[0].should eql(RubyQuiz1::Card.new deck, number: 1, face: 'C')
+    deck.cards[1].should eql(RubyQuiz1::Card.new deck, number: 2, face: 'C')
+    deck.cards[50].should eql(RubyQuiz1::Card.new deck, number: 12, face: 'S')
+    deck.cards[51].should eql(RubyQuiz1::Card.new deck, number: 13, face: 'S')
+    deck.cards[52].should eql(RubyQuiz1::Card.new deck, face: 'J')
+    deck.cards[53].should eql(RubyQuiz1::Card.new deck, face: 'J')
   end  
-  it "should randomize 52 cards"
+  it "should randomize 52 cards" do
+    # test 5 cards at random, it should not be in its default positions"
+    pending
+  end  
 end
+
+
 
 
